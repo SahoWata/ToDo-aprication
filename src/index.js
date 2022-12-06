1075b28042302ebc68067bee8d9673b3a2afaa29
SahoWata <85483830+SahoWata@users.noreply.github.com>
2022-12-06 10:59:31 +0900

    JSボタン機能完成 createIncompleteList関数を作成し、処理をまとめた

--- src/index.js
+++ src/index.js
@@ -5,8 +5,17 @@ const onClickAdd = () => {
   //テキストボックスの値を取得し、その後テキストボックスを初期化する
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-  //alert(inputText);
 
+  createIncompleteList(inputText);
+};
+
+//未完了リストから指定の要素を削除する
+const deleteFormIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+//未完了リストに追加する関数
+const createIncompleteList = (text) => {
   //li生成
   const li = document.createElement("li");
   li.className = "list-row";
@@ -14,7 +23,7 @@ const onClickAdd = () => {
 
   //pタグ生成
   const p = document.createElement("p");
-  p.innerText = inputText;
+  p.innerText = text;
   //console.log(p);
 
   //button（完了）タグ生成
@@ -48,6 +57,7 @@ const onClickAdd = () => {
       //テキストを取得
       const text = backButton.parentNode.firstElementChild.innerText;
       //console.log(text);
+      createIncompleteList(text);
     });
 
     //liタグの子要素に各要素を設定
@@ -77,11 +87,6 @@ const onClickAdd = () => {
 
   //未完了リストに追加する
   document.getElementById("incomplete-list").appendChild(li);
-};
-
-//未完了リストから指定の要素を削除する
-const deleteFormIncompleteList = (target) => {
-  document.getElementById("incomplete-list").removeChild(target);
 };
 
 document
